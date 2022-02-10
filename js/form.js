a2190c449f8f6773120674ef1ff89bb931b9281c
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-02-10 12:26:04 +0530

    Update form.js

--- js/form.js
+++ js/form.js
@@ -4,10 +4,10 @@ class Form {
     this.input = createInput("name");
     this.buttonImg = createImg("./images/start.png");
   }
-  display() {
-    this.input.position(displayWidth / 2 - 90, displayHeight / 2 - 150);
+ display() {
+    this.input.position(380, 180);
     this.input.size(180, 30);
-    this.buttonImg.position(displayWidth / 2 +50 , displayHeight / 2 - 100);
+    this.buttonImg.position(475 + 50, 250);
     this.buttonImg.size(150, 50);
 
     this.buttonImg.mousePressed(() => {
@@ -18,10 +18,11 @@ class Form {
       player.updateCount(playerCount);
       player.index = playerCount;
       player.addPlayer();      
-      this.greetings.html("Hello " + player.name +", Let us wait for others to join...");
-      this.greetings.position(displayWidth / 2 - 200, displayHeight / 2 - 150);
+      this.greetings.html("Hello " + player.name);
+      this.greetings.position(300, 180);
     });
   }
+
 
   hide() {
     this.buttonImg.hide();
