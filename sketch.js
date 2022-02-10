77ccaa8f3202c9e4192e7016c511c8f2499ad153
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-02-10 12:25:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,7 +14,7 @@ function preload() {
 
 function setup() {
   database = firebase.database();
-  canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(950, 470);
 
   game = new Game();
   game.start();
@@ -26,6 +26,3 @@ function draw() {
   }
 }
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
-}
